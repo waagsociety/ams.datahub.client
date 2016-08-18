d01d5dbc36a65c25ac93d97211126316bf048385
Nathan Uphoff <nathan@Administrators-MacBook-Pro.local>
2016-08-18 14:32:52 +0200

    BasePath test for server deploy

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 A client interface for the AMS data platform that runs in the browser using React, Redux, and MapBox.
 
 - Run `npm install` to get started.
-- To develop run `npm run dev`, the app is hosted at http://localhost:8080/
+- Run `npm run dev` to run a development version, hosted at http://localhost:8080/
 - Run `npm run build` to create a new build in /dist
--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,18 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import { Provider } from "react-redux"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 
-import store from "./store"
-import { Browse, Results } from './pages' 
+import store from './store'
+import { Browse, Results } from './pages'
 
 const history = syncHistoryWithStore(browserHistory, store);
 
 ReactDOM.render(
 	<Provider store={store}>
 		<Router history={history}>
-			<Route path='/' component={Browse}></Route>
+			<Route path='/ams/amsclient/' component={Browse}></Route>
 		</Router>
 	</Provider>,
 document.querySelector('#app'))
