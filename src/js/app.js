4b98efa767dcff04c4c4a169de784dc04e0e87b5
Nathan Uphoff <nathan@Administrators-MacBook-Pro.local>
2016-08-18 14:46:14 +0200

    set basePath to /browse-data pt.2

--- src/js/app.js
+++ src/js/app.js
@@ -8,12 +8,12 @@ import store from './store'
 import { Browse, Results } from './pages'
 
 const history = syncHistoryWithStore(browserHistory, store);
-const deployBase = '/ams/browse-data'
+// const deployBase = '/ams/browse-data'
 
 ReactDOM.render(
 	<Provider store={store}>
 		<Router history={history}>
-			<Route path={deployBase + '/'} component={Browse}></Route>
+			<Route path='/ams/browse-data/' component={Browse}></Route>
 			<Route path='/' component={Browse}></Route>
 		</Router>
 	</Provider>,
