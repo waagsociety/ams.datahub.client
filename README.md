54f085751d7a7f2332e45506900b1f70e3e338bc
Nathan Uphoff <nathan@Administrators-MacBook-Pro.local>
2016-08-18 13:51:06 +0200

    Added a Readme and build script: npm run build

---
+++ README.md
@@ -0,0 +1,6 @@
+# datahub_browser_front-end
+The datahub browser will be a browser to view different datasets of the AMR.
+
+- `npm install`
+- `npm run dev`: runs development version at http://localhost:8080/
+- `npm run build`: creates and populates dist/ in the project root.
---
+++ webpack.production.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  context: __dirname + '/src',
+  devtool: null,
+  entry: "./js/app.js",
+  output: {
+    path: __dirname + '/dist',
+    filename: 'app.min.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
+        }
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader?modules&localIdentName=[hash:base64:5]!postcss-loader',
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: 'file-loader',
+      },
+      {
+        test: /\.js$/,
+        include: path.resolve('node_modules/mapbox-gl-shaders/index.js'),
+        loader: 'transform/cacheable?brfs',
+      },
+    ],
+    postLoaders: [{
+      include: /node_modules\/mapbox-gl-shaders/,
+      loader: 'transform',
+      query: 'brfs',
+    }],
+  }
+};
