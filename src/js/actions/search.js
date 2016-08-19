59b6180f627acb6afecc017f9d871badfad88621
Nathan Uphoff <nathan@Administrators-MacBook-Pro.local>
2016-08-19 14:40:12 +0200

    Added a request from postman

--- src/js/actions/search.js
+++ src/js/actions/search.js
@@ -4,25 +4,27 @@ export const search = {
 
 	query: (value) => (dispatch) => {
 
-    axios({
-      method: 'post',
-      url: 'http://138.201.141.84/rest/items/find-by-metadata-field',
-      data: {
-        key: "dc.contributor.author",
-        value: "Gemeente Amsterdam",
-      },
-      withCredentials: true,
-      headers: {
-        'accept': 'application/json',
-        'content-type': 'application/json',
-        'cache-control': 'no-cache',
-        'postman-token': '4b888458-e05f-1482-ca2b-be32d2794a06',
-      },
-    }).then(response => {
-      console.log(response)
-    }).catch(error => {
-      console.info(error)
-    })
+    findByMetaData();
+
+    // axios({
+    //   method: 'post',
+    //   url: 'http://138.201.141.84/rest/items/find-by-metadata-field',
+    //   data: {
+    //     key: "dc.contributor.author",
+    //     value: "Gemeente Amsterdam",
+    //   },
+    //   withCredentials: true,
+    //   headers: {
+    //     'accept': 'application/json',
+    //     'content-type': 'application/json',
+    //     'cache-control': 'no-cache',
+    //     'postman-token': '4b888458-e05f-1482-ca2b-be32d2794a06',
+    //   },
+    // }).then(response => {
+    //   console.log(response)
+    // }).catch(error => {
+    //   console.info(error)
+    // })
 
     axios.get('http://138.201.141.84/rest/items')
       .then(response => {
@@ -51,5 +53,32 @@ export const search = {
     })  
 
   },
+
+}
+
+
+
+function findByMetaData() {
+
+  var data = JSON.stringify({
+    "key": "dc.contributor.author",
+    "value": "Gemeente Amsterdam"
+  });
+
+  var xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === 4) console.log(this.responseText);
+  });
+
+  xhr.open("POST", "http://138.201.141.84/rest/items/find-by-metadata-field");
+  xhr.setRequestHeader("rest-dspace-token", "faeae1f3-4880-41ef-ae26-cca6f10229ca");
+  xhr.setRequestHeader("content-type", "application/json");
+  xhr.setRequestHeader("accept", "application/json");
+  xhr.setRequestHeader("cache-control", "no-cache");
+  xhr.setRequestHeader("postman-token", "51d36325-7693-2d45-4736-ea90f3c5c28f");
+
+  xhr.send(data);
 
 }
