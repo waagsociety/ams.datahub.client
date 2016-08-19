fee33046c6137d852e7ff976b5aa1e35ad8b05a4
Nathan Uphoff <nathan@Administrators-MacBook-Pro.local>
2016-08-19 13:01:51 +0200

    Fetch attempt with another request body

--- src/js/actions/search.js
+++ src/js/actions/search.js
@@ -4,30 +4,13 @@ export const search = {
 
 	query: (value) => (dispatch) => {
 
-    var data = JSON.stringify({
-    "key": "dc.contributor",
-    "value": "Bocconi, Stefano"
-    });
-
-    // var xhr = new XMLHttpRequest();
-    // xhr.withCredentials = true;
-
-    // xhr.addEventListener("readystatechange", function () {
-    // if (this.readyState === 4) {
-    //   console.log(this.responseText);
-    // }
-    // });
-
-    // xhr.open("POST", "http://138.201.141.84/rest/items/find-by-metadata-field");
-    // xhr.setRequestHeader("accept", "application/json");
-    // xhr.setRequestHeader("content-type", "application/json");
-    // xhr.setRequestHeader("cache-control", "no-cache");
-    // xhr.setRequestHeader("postman-token", "4b888458-e05f-1482-ca2b-be32d2794a06");
-
-    // xhr.send(data);
-
-    const config = {
-      data: data,
+    axios({
+      method: 'post',
+      url: 'http://138.201.141.84/rest/items/find-by-metadata-field',
+      data: {
+        key: "dc.contributor",
+        value: "Bocconi, Stefano",
+      },
       withCredentials: true,
       headers: {
         'accept': 'application/json',
@@ -35,15 +18,11 @@ export const search = {
         'cache-control': 'no-cache',
         'postman-token': '4b888458-e05f-1482-ca2b-be32d2794a06',
       },
-    }
-
-    axios.post('http://138.201.141.84/rest/items/find-by-metadata-field', config)
-      .then(response => {
-        console.log(response)
-      })
-      .catch(error => {
-        console.info(error)
-      })
+    }).then(response => {
+      console.log(response)
+    }).catch(error => {
+      console.info(error)
+    })
 
     const headers = { 'Content-Type': 'application/json' }
     axios.get('http://138.201.141.84/rest/items', { headers })
