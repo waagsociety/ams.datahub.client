db6bbd9ba5c60f1f2a0031cbae8ff6721d94993d
Nathan Uphoff <nathan@Administrators-MacBook-Pro.local>
2016-08-24 09:20:16 +0200

    Added some styling and a faux placeholder MapBox

--- src/js/components/QueryTags/index.js
+++ src/js/components/QueryTags/index.js
@@ -14,13 +14,11 @@ export const QueryTags = ({ props }) => {
     const { onClick } = eventHandlers(props)
 	const { query } = props.store
 
-    console.log(query)
-
 	const Tag = ({ key, value }, i) =>
 		<button className='tag' key={i} name={key} value={value} onClick={onClick}>{value}</button>
 
 	const TagList = (query) => 
-		<div class='tags'>{ [ ...query ].reverse().map((item, i) => Tag(item, i)) }</div>
+		<div class='breadcrumb tags'>{ [ ...query ].reverse().map((item, i) => Tag(item, i)) }</div>
 	
 	if (query.length) return TagList(query)
 	else return <h1>QueryTags</h1>
