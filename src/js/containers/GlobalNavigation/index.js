765d7b52181a6c8d0479f9bb72cf4cda3228cf81
Nathan Uphoff <nathan@Administrators-MacBook-Pro.local>
2016-09-01 15:06:29 +0200

    fixed href to the home page

--- src/js/containers/GlobalNavigation/index.js
+++ src/js/containers/GlobalNavigation/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function GlobalNavigation({ props }) {
 
   return <nav class='page global'>
-    <a class='logo' href="./">datahub</a>
+    <a class='logo' href="http://138.201.141.84/ams/">datahub</a>
     <ul>
       <li><a href="http://138.201.141.84/ams/information">Information</a></li>
       <li><a href="http://138.201.141.84/ams/browse-data" class='active'>Browse data</a></li>
