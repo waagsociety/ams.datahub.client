1b61e5e6edaebb9ce04a07a9a761fa92278a92ec
Nathan Uphoff <nathan@Administrators-MacBook-Pro.local>
2017-01-11 10:28:58 +0100

    test env weppack plugin

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -2,6 +2,15 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
+  plugins: [
+    new webpack.DefinePlugin({
+      process: {
+        env: {
+          NODE_ENV: "production"
+        }
+      }
+    }),
+  ],
   context: __dirname + '/src',
   devtool: null,
   entry: "./js/app.js",
