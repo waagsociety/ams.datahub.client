be82ec467fa18e439b4b2baa27dc0e51ccbcc8b3
Nathan Uphoff <nathan@Administrators-MacBook-Pro.local>
2017-04-04 15:40:24 +0200

    invalid URL’s are filtered out

--- src/js/components/ItemTool/index.js
+++ src/js/components/ItemTool/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import validUrl from 'valid-url'
+
+console.log(validUrl)
 
 export default function ItemTool({ data }) {
   
   const { source } = data
+  const sources = source.filter(validUrl.is_uri)
 
   return <section className='ItemTool content'>
     
     <h1>Tool</h1>
     
-    { source.length ? <a href={source} target='_blank'>Get this Tool</a> : null }
+    { sources.length ? <a href={sources[0]} target='_blank'>Get this Tool</a> : null }
 
   </section>
   
